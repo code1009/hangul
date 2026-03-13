426d50bd2e2276e23e6fb92bdbc862e0cc839af3
code1009 <code1009@naver.com>
2026-03-13 21:46:04 +0900

    1

--- hangul/src/app/app_bf.cpp
+++ hangul/src/app/app_bf.cpp
@@ -3,15 +3,93 @@
 #include <iostream>
 #include <vector>
 
+#include <Windows.h>
+
 #include "../lib/bf/bf.h"
+#include "../lib/krc/krc.h"
 
 
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+std::wstring mbcs_to_wcs(std::string input, UINT codepage)
+{
+	int len = MultiByteToWideChar(codepage, 0, input.c_str(), -1, NULL, 0);
+
+
+	if (len > 0)
+	{
+		std::vector<wchar_t> buf(len);
+
+
+		MultiByteToWideChar(codepage, 0, input.c_str(), -1, &buf[0], len);
+
+		return std::wstring(&buf[0]);
+	}
+
+	return std::wstring();
+}
+
+std::string wcs_to_mbcs(std::wstring input, UINT codepage)
+{
+	int len = WideCharToMultiByte(codepage, 0, input.c_str(), -1, NULL, 0, NULL, NULL);
+
+
+	if (len > 0)
+	{
+		std::vector<char> buf(len);
+
+
+		WideCharToMultiByte(codepage, 0, input.c_str(), -1, &buf[0], len, NULL, NULL);
+
+		return std::string(&buf[0]);
+	}
+
+	return std::string();
+}
+
+std::string utf8_to_mbcs(std::string /*input*/utf8, UINT codepage)
+{
+	//	std::string  utf8 ;
+	std::wstring utf16;
+	std::string  mbcs;
+
+
+	//	utf8  = input;
+	utf16 = mbcs_to_wcs(utf8, CP_UTF8);
+	mbcs = wcs_to_mbcs(utf16, codepage);
+
+	return mbcs;
+}
+
+std::string mbcs_to_utf8(std::string /*input*/mbcs, UINT codepage)
+{
+	std::string  utf8;
+	std::wstring utf16;
+	//	std::string  mbcs ;
+
+
+	//	mbcs  = input;
+	utf16 = mbcs_to_wcs(mbcs, codepage);
+	utf8 = wcs_to_mbcs(utf16, CP_UTF8);
+
+	return utf8;
+}
+
+std::string wcs_to_utf8(std::wstring input)
+{
+	return wcs_to_mbcs(input, CP_UTF8);
+}
+
+std::wstring utf8_to_wcs(std::string input)
+{
+	return mbcs_to_wcs(input, CP_UTF8);
+}
 
 
 
 /////////////////////////////////////////////////////////////////////////////
 //===========================================================================
-class text_bitmap
+class text_canvas
 {
 private:
 	uint32_t _cx;
@@ -19,7 +97,7 @@ private:
 	std::vector<std::string> _bitmap_buffer;
 
 public:
-	text_bitmap() :
+	text_canvas() :
 		_cx(128u), _cy(32u)
 	{
 		setup();
@@ -35,6 +113,7 @@ private:
 		}
 	}
 
+public:
 	void clear(void)
 	{
 		for (uint32_t y = 0u; y < _cy; y++)
@@ -88,7 +167,7 @@ public:
 		}
 	}
 
-	void draw_string(const uint32_t start_x, const uint32_t start_y, const char* str)
+	void draw_cp949_string(const uint32_t start_x, const uint32_t start_y, const char* str)
 	{
 		bf_context_t* ctx = bf_context_default_get();
 		bf_font_bitmap_t font_bitmap;
@@ -108,7 +187,7 @@ public:
 				if (*src == '\n')
 				{
 					x = start_x;
-					y += 16u;
+					y += (font_bitmap_drawn ? font_bitmap.font_bitmap_cy : 16u);
 					src += 1;
 					continue;
 				}
@@ -165,13 +244,184 @@ public:
 			x += font_bitmap.font_bitmap_cx;
 		}
 	}
+
+	void draw_unicode_string(const uint32_t start_x, const uint32_t start_y, const wchar_t* str)
+	{
+		bf_context_t* ctx = bf_context_default_get();
+		bf_font_bitmap_t font_bitmap;
+
+		uint32_t x = start_x;
+		uint32_t y = start_y;
+
+		const wchar_t* src = str;
+		bool font_bitmap_drawn = false;
+
+		while (*src)
+		{
+			bf_uint16_t char_code;
+			if (*src == '\n')
+			{
+				x = start_x;
+				y += (font_bitmap_drawn ? font_bitmap.font_bitmap_cy : 16u);
+				src += 1;
+				continue;
+			}
+			else if (*src == '\r')
+			{
+				x = start_x;
+				src += 1;
+				continue;
+			}
+			else if (*src == '\t')
+			{
+				x += 8 * 8u;
+				src += 1;
+				continue;
+			}
+			else if (*src == '\b')
+			{
+				x -= (font_bitmap_drawn ? font_bitmap.font_bitmap_cx : 8u);
+				src += 1;
+				continue;
+			}
+			else if (*src < 0x20)
+			{
+				char_code = '?';
+				src += 1;
+			}
+			else if (*src == 0x7F)
+			{
+				char_code = '?';
+				src += 1;
+			}
+			else
+			{
+				char_code = (bf_uint16_t)*src;
+				src += 1;
+			}
+
+			bf_get_unicode_bitmap(ctx, char_code, &font_bitmap);
+			draw_font_bitmap(start_x + x, start_y + y, &font_bitmap);
+			x += font_bitmap.font_bitmap_cx;
+		}
+	}
+
+	void draw_utf8_string(const uint32_t start_x, const uint32_t start_y, const uint8_t* str)
+	{
+		bf_context_t* ctx = bf_context_default_get();
+		bf_font_bitmap_t font_bitmap;
+
+		uint32_t x = start_x;
+		uint32_t y = start_y;
+
+		const uint8_t* src = str;
+		bool font_bitmap_drawn = false;
+
+		while (*src)
+		{
+			uint8_t utf8_char;
+			const uint8_t* utf8_char_pointer = NULL;
+			uint32_t utf8_char_length = 0;
+
+			if (*src == '\n')
+			{
+				x = start_x;
+				y += (font_bitmap_drawn ? font_bitmap.font_bitmap_cy : 16u);
+				src += 1;
+				continue;
+			}
+			else if (*src == '\r')
+			{
+				x = start_x;
+				src += 1;
+				continue;
+			}
+			else if (*src == '\t')
+			{
+				x += 8 * 8u;
+				src += 1;
+				continue;
+			}
+			else if (*src == '\b')
+			{
+				x -= (font_bitmap_drawn ? font_bitmap.font_bitmap_cx : 8u);
+				src += 1;
+				continue;
+			}
+			else if (*src < 0x20)
+			{
+				utf8_char = '?';
+				src += 1;
+
+				utf8_char_pointer = &utf8_char;
+				utf8_char_length = 1;
+			}
+			else if (*src == 0x7F)
+			{
+				utf8_char = '?';
+				src += 1;
+
+				utf8_char_pointer = &utf8_char;
+				utf8_char_length = 1;
+			}
+			else
+			{
+				utf8_char_pointer = (uint8_t*)src;
+				utf8_char_length = (uint32_t)(src - str);
+
+				krc_wchar32_t unicode_char;
+				krc_size_t consumed_bytes;
+				consumed_bytes = krc_utf8_to_unicode_char(
+					(const krc_char_t*)utf8_char_pointer,
+					(krc_size_t)utf8_char_length,
+					&unicode_char
+				);
+				if (consumed_bytes == 0)
+				{
+					utf8_char = '?';
+					utf8_char_pointer = &utf8_char;
+					utf8_char_length = 1;
+
+					src += 1;
+				}
+				else
+				{
+					src += consumed_bytes;
+				}
+
+			}
+
+			bf_get_utf8_bitmap(ctx, utf8_char_pointer, utf8_char_length , &font_bitmap);
+			draw_font_bitmap(start_x + x, start_y + y, &font_bitmap);
+			x += font_bitmap.font_bitmap_cx;
+		}
+	}
 };
 
 static void test1()
 {
-	text_bitmap tb;
-	tb.draw_string(1u, 0u, "A한글\n!ф詰");
-	tb.output();
+	text_canvas c;
+
+
+	c.draw_cp949_string(1u, 0u, "A한글\n!ф詰");
+	c.output();
+
+
+	printf("\r\n\r\n");
+
+
+	c.clear();
+	c.draw_unicode_string(1u, 0u, L"b유니코드\n!ф詰");
+	c.output();
+
+
+	printf("\r\n\r\n");
+
+
+	c.clear();
+	std::string utf8 = mbcs_to_utf8("c유니코드\n!ф詰", CP_ACP);
+	c.draw_utf8_string(1u, 0u, (const uint8_t*)utf8.c_str());
+	c.output();
 }
 
 void bf_main()
--- hangul/src/lib/bf/bf_char_bitmap.h
+++ hangul/src/lib/bf/bf_char_bitmap.h
@@ -32,7 +32,7 @@ BF_API void bf_get_unicode_bitmap(
 BF_API void bf_get_utf8_bitmap(
 	bf_context_t* ctx,
 	const bf_uint8_t* char_code_pointer,
-	const bf_uint8_t char_code_length,
+	const bf_uint32_t char_code_length,
 	bf_font_bitmap_t* font_bitmap
 );
 
